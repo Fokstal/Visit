29f7cf5e53d0c49296006a573f40fd726df7e05b
Fokstal <klabu2016@mail.ru>
2023-10-07 10:36:17 +0300

    feat: add anchor link (Sat, 07.10 10:40 AM)

--- script.js
+++ script.js
@@ -6,8 +6,17 @@ window.addEventListener('scroll', function () {
         this.document.querySelector("header").classList.add("header-b");
         return;
     }
-    
+
     this.document.querySelector("header").classList.remove("header-b");
+});
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
 
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'center'
+        });
+    });
 });
